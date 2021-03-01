3902937bbfcec960b4c7a8820589c3b278adb9e4
Ellen Christina <ellencas172@gmail.com>
2021-03-01 21:09:37 +0000

    guia repetição e condicional

---
+++ js/rep.js
@@ -0,0 +1,19 @@
+var idade = prompt ("qual sua idade? "); //promot define uma pergunta para o usuário e armazena em uma variável
+if (idade >= 18){
+    alert("maior de idade");
+}else {
+    alert("menor de idade");
+}
+
+var count = 0;
+console.log("WHILE");
+while (count <= 5){
+    console.log(count);
+    count++;
+};
+
+var num;
+console.log("FOR");
+for (num=5; num >= 0; num--){
+    console.log(num);
+};
