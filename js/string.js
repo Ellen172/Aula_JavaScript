7fe6b7e652e646fd45e92043e50293f03f64c083
Ellen Christina <ellencas172@gmail.com>
2021-03-01 20:27:39 +0000

    criação da página string

---
+++ js/string.js
@@ -0,0 +1,7 @@
+var frase = "Japão é o melhor time do mundo";
+
+//funções string
+console.log("Frase original: " + frase);
+console.log("replace: " + frase.replace("Japão", "Brasil")); //troca temporariamente o 1º elemento pelo 2º
+console.log("toUpperCase: " + frase.toUpperCase()); //transforma em maiuscula
+console.log("toLowerCase: " + frase.toLowerCase()); //transforma em minuscula
